1d5b63170bfabf3729812a219f2a20d478826487
neilfedillaga <fedillagadan@gmail.com>
2016-07-09 23:46:09 +0800

    Deleted student folder
    Student folder is a trial file

--- wp-content/plugins/student/student/js/custom-quicktags.js
+++
@@ -1,9 +0,0 @@
-// jQuery(document).ready(function() {
-
-// 	quicktags({
-// 		id: 'wp_custom_attachment',
-// 		buttons: "em,strong,link,ul,li"
-// 	});
-	 
-
-// });
--- wp-content/plugins/student/student/templates/single-student.php
+++
@@ -1,6 +0,0 @@
-
-<?php
-/**
-*Template: template1
-*/
-echo 'I am Dabert';
